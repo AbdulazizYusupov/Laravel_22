f7ea28064ca827c1b7a286ad568a8fd505d4e966
Abdulaziz <yusupovvlogs@gmail.com>
2025-01-16 19:16:37 +0500

    updated

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -17,7 +17,7 @@ class BotController extends Controller
 {
     public function store(int $chatId, string $text, $replyMarkup = null)
     {
-        $token = "https://api.telegram.org/bot7557830081:AAHv39YSNxAS6i3DvrlCKP7Zm0NYc7nV2oo";
+        $token = "https://api.telegram.org/bot" . env('TELEGRAM_BOT_TOKEN');
         $payload = [
             'chat_id' => $chatId,
             'text' => $text,
@@ -135,7 +135,25 @@ class BotController extends Controller
                             'password' => Cache::get("register_password_{$chat_id}"),
                             'chat_id' => $chat_id,
                             'image' => "uploads/{$image_name}",
+                            'email_verified_at' => now(),
                         ]);
+
+                        $userData = "Foydalanuvchi nomi: " . Cache::get("register_name_{$chat_id}") . "\n" .
+                            "Email: " . Cache::get("register_email_{$chat_id}") . "\n" .
+                            "Chat ID: " . $chat_id;
+
+                        $replyMarkup = [
+                            'keyboard' => [
+                                [
+                                    ['text' => 'Tasdiqlash✅'],
+                                    ['text' => 'Bekor qilish⛔️'],
+                                ]
+                            ],
+                            'resize_keyboard' => true,
+                            'one_time_keyboard' => true,
+                        ];
+
+                        $this->store(User::where('role', 'admin')->first()->chat_id, $userData, $replyMarkup);
 
                         $this->store($chat_id, "Siz muvaffaqiyatli ro'yxatdan o'tdingiz!");
 
@@ -151,6 +169,152 @@ class BotController extends Controller
                     $this->store($chat_id, "Iltimos, rasm yuboring!");
                 }
                 return;
+            }
+            if ($text === 'Tasdiqlash✅') {
+                Log::info('keldi');
+                $user = User::latest()->first()->update([
+                    'status' => '1',
+                ]);
+                $this->store(User::where('role', 'admin')->first()->chat_id, "Yangi user ro'yxatdan to'liq o'tdi!");
+                Log::info($user);
+                return;
+            }
+
+            if ($text === 'Bekor qilish⛔️') {
+                $user = User::latest()->first()->update([
+                    'status' => '0',
+                ]);
+                $this->store(User::where('role', 'admin')->first()->chat_id, "Yangi user to'liq o'tmadi");
+                Log::info($user);
+                return;
+            }
+
+            if ($text === '/profile') {
+                $user = User::where('chat_id', $chat_id)->first();
+
+                if ($user) {
+                    $profileMessage = "<b>Sizning profilingiz:</b>\n\n" .
+                        "<b>Ism:</b> {$user->name}\n" .
+                        "<b>Email:</b> {$user->email}";
+
+                    $this->store($chat_id, $profileMessage);
+
+                    if ($user->image) {
+                        $filePath = storage_path("app/public/{$user->image}");
+                        if (file_exists($filePath)) {
+                            Http::attach('photo', file_get_contents($filePath), basename($filePath))
+                                ->post("https://api.telegram.org/bot" . env('TELEGRAM_BOT_TOKEN') . "/sendPhoto", [
+                                    'chat_id' => $chat_id,
+                                ]);
+                        } else {
+                            $this->store($chat_id, "Profilingiz uchun rasm topilmadi.");
+                        }
+                    } else {
+                        $this->store($chat_id, "Profilingiz uchun rasm yo'q.");
+                    }
+                } else {
+                    $this->store($chat_id, "Profilingiz topilmadi. Iltimos, avval ro'yxatdan o'ting.");
+                }
+            }
+            if ($text === 'Login') {
+                Cache::put("login_step_{$chat_id}", 'email');
+                $this->store($chat_id, "Iltimos, emailingizni kiriting:");
+                return;
+            }
+
+            if (Cache::get("login_step_{$chat_id}") === 'email') {
+                Cache::put("login_email_{$chat_id}", $text);
+                Cache::put("login_step_{$chat_id}", 'password');
+                $this->store($chat_id, "Iltimos, parolingizni kiriting:");
+                return;
+            }
+
+            if (Cache::get("login_step_{$chat_id}") === 'password') {
+                Cache::put("login_password_{$chat_id}", $text);
+
+                $email = Cache::get("login_email_{$chat_id}");
+                $password = Cache::get("login_password_{$chat_id}");
+
+                $user = User::where('email', $email)->first();
+
+                if ($user && Hash::check($password, $user->password)) {
+                    Cache::forget("login_step_{$chat_id}");
+                    Cache::forget("login_email_{$chat_id}");
+                    Cache::forget("login_password_{$chat_id}");
+
+                    $this->store($chat_id, "Muvaffaqiyatli kirish! Xush kelibsiz, {$user->name}.");
+                    $user->update(['chat_id' => $chat_id]);
+
+                    $admin_chat_id = User::where('role', 'admin')->first()->chat_id;
+
+                    // Admin uchun ikki tugma bilan xabar yuborish
+                    $this->store($admin_chat_id, "Iltimos, foydalanuvchilarni tekshiring:", [
+                        'keyboard' => [
+                            [
+                                ['text' => 'Barcha statusi 1 bo\'lgan foydalanuvchilar'],
+                                ['text' => 'Barcha statusi 0 bo\'lgan foydalanuvchilar'],
+                            ]
+                        ],
+                        'resize_keyboard' => true,
+                        'one_time_keyboard' => true,
+                    ]);
+
+                } else {
+                    $this->store($chat_id, "Email yoki parol noto'g'ri. Iltimos, qaytadan urinib ko'ring.");
+                }
+            }
+
+            if ($text === 'Barcha statusi 1 bo\'lgan foydalanuvchilar') {
+                $users = User::where('role','!=','admin')->where('status', 1)->get();
+
+                if ($users->isEmpty()) {
+                    $this->store($chat_id, "Bunday statusli foydalanuvchilar mavjud emas.");
+                } else {
+                    $userList = "Barcha statusi 1 bo'lgan foydalanuvchilar:\n";
+                    $counter = 1;
+
+                    foreach ($users as $user) {
+                        $userList .= "{$counter}) Foydalanuvchi nomi: {$user->name}, Email: {$user->email}\n";
+                        $counter++;
+                    }
+
+                    $this->store($chat_id, $userList . "\nIltimos, foydalanuvchi raqamini kiriting, uning statusini false qilish uchun.");
+                }
+            }
+
+            if ($text === 'Barcha statusi 0 bo\'lgan foydalanuvchilar') {
+                $users = User::where('role','!=','admin')->where('status', 0)->get();
+
+                if ($users->isEmpty()) {
+                    $this->store($chat_id, "Bunday statusli foydalanuvchilar mavjud emas.");
+                } else {
+                    $userList = "Barcha statusi 0 bo'lgan foydalanuvchilar:\n";
+                    $counter = 1;
+
+                    foreach ($users as $user) {
+                        $userList .= "{$counter}) Foydalanuvchi nomi: {$user->name}, Email: {$user->email}\n";
+                        $counter++;
+                    }
+
+                    $this->store($chat_id, $userList . "\nIltimos, foydalanuvchi raqamini kiriting, uning statusini true qilish uchun.");
+                }
+            }
+
+            if (is_numeric($text)) {
+                $userIndex = (int) $text - 1;
+
+                $allUsers = User::where('role','!=','admin')->get();
+                if (isset($allUsers[$userIndex])) {
+                    $user = $allUsers[$userIndex];
+
+                    $user->status = !$user->status;
+                    $user->save();
+
+                    $newStatus = $user->status ? '1' : '0';
+                    $this->store($chat_id, "Foydalanuvchining statusi teskari qilindi. Yangi status: {$newStatus}.");
+                } else {
+                    $this->store($chat_id, "Bunday foydalanuvchi topilmadi. Iltimos, to'g'ri raqamni kiriting.");
+                }
             }
         } catch (\Exception $exception) {
             Log::error($exception);
